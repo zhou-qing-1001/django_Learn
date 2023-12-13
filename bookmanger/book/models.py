b7f0f171dc2017f4107c5e8902e44d25a354ad40
zhou <166125541@qq.com>
2023-12-13 20:51:29 +0800

    orm模型 生成迁移文件，执行迁移文件，产生数据表

--- bookmanger/book/models.py
+++ bookmanger/book/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+"""
+继承models.Model
+自动添加主键(id)
+字段
+
+    字段名=model.类型(选项)
+    
+"""
+
+
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
 # Create your models here.
